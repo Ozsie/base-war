8174d87407ba1f920267426ece414a25440f111b
Ozsie <oscar@djupfeldt.se>
2017-01-11 13:08:56 +0100

    Removed target

--- target/docker/Dockerfile
+++
@@ -1,2 +0,0 @@
-FROM ozsie/wildfly-base
-COPY ./target/base.war $DEPLOYMENTS
--- target/docker/src/main/java/com/airhacks/JAXRSConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.airhacks;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("resources")
-public class JAXRSConfiguration extends Application {
-
-}
